0d26a653663ec698c4cdf2fc0bbe03b010dc8467
leo-mutuku <le.mutuku@gmail.com>
2024-08-27 21:13:11 +0300

    update

--- src/components/togglebutton/ToggleButton.tsx
+++ src/components/togglebutton/ToggleButton.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
-import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
-import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+
 import Stack from "@mui/material/Stack";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import NightlightRoundOutlinedIcon from "@mui/icons-material/NightlightRoundOutlined";
 
 export default function ToggleButtonNotEmpty() {
   const [alignment, setAlignment] = React.useState("left");
@@ -26,14 +26,12 @@ export default function ToggleButtonNotEmpty() {
         onChange={handleAlignment}
         aria-label="text alignment"
       >
-        <ToggleButton value="left" aria-label="left aligned">
-          <FormatAlignLeftIcon />
+        <ToggleButton value="left" aria-label="left aligned" size="small">
+          <LightModeOutlinedIcon />
         </ToggleButton>
-        <ToggleButton value="center" aria-label="centered">
-          <FormatAlignCenterIcon />
-        </ToggleButton>
-        <ToggleButton value="right" aria-label="right aligned">
-          <FormatAlignRightIcon />
+
+        <ToggleButton value="right" aria-label="right aligned" size="small">
+          <NightlightRoundOutlinedIcon />
         </ToggleButton>
       </ToggleButtonGroup>
     </Stack>
