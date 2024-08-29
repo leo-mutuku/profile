9631adb420f6cb7dc44a04dae8135240f84bd9a3
leo-mutuku <le.mutuku@gmail.com>
2024-08-29 05:47:41 +0300

    update

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import StepperComponent from "../../components/steppercomponent";
 
@@ -7,56 +7,110 @@ type HomePageProps = {
 };
 const HomePage = ({ themeValue }: HomePageProps) => {
   return (
-    <Grid
-      container
-      gap={3}
-      sx={{
-        background: themeValue === "dark" ? "black" : "white",
-        padding: "15px 30px",
-        minHeight: "100vh",
-      }}
-    >
+    <>
       <Grid
-        item
+        container
+        gap={3}
         sx={{
-          flex: { xs: "1 0 15rem", md: "1 0 15rem" },
-          height: { xs: "5rem", md: "5rem" },
-          background: "transparent",
-          display: "flex", // Enable flexbox
-          justifyContent: "center", // Center horizontally
-          alignItems: "center", // Center vertically
+          background: themeValue === "dark" ? "black" : "white",
+          padding: "15px 30px",
+          minHeight: "100vh",
         }}
       >
-        <Box
-          component="img"
-          src="/profile.webp" // Path to your image in the public folder
-          alt="Description of image"
+        <Grid
+          item
           sx={{
-            marginTop: "60%",
-            borderRadius: "50%", // Makes the image circular
-            width: "200px", // Fixed width
-            height: "200px", // Fixed height
-            objectFit: "cover", // Ensures the image covers the area while maintaining aspect ratio
+            flex: { xs: "1 0 15rem", md: "1 0 15rem" },
+            height: { xs: "5rem", md: "5rem" },
+            background: "transparent",
+            display: "flex", // Enable flexbox
+            justifyContent: "center", // Center horizontally
+            alignItems: "center", // Center vertically
           }}
-        />
-        <Box sx={{}} />
+        >
+          <Box
+            component="img"
+            src="/profile.webp" // Path to your image in the public folder
+            alt="Description of image"
+            sx={{
+              marginTop: "60%",
+              borderRadius: "50%", // Makes the image circular
+              width: "200px", // Fixed width
+              height: "200px", // Fixed height
+              objectFit: "cover", // Ensures the image covers the area while maintaining aspect ratio
+            }}
+          />
+          <Box sx={{}} />
+        </Grid>
+        <Grid
+          item
+          sx={{
+            flex: { xs: "1 0 15rem", md: "1 0 15rem" },
+            height: { xs: "5rem", md: "5rem" },
+            background: "transparent",
+            display: "flex", // Enable flexbox
+            justifyContent: "center", // Center horizontally
+            alignItems: "center", // Center vertically
+          }}
+        >
+          <Box sx={{ marginTop: "60%" }}>
+            <Typography>Skills</Typography>
+            <StepperComponent />
+          </Box>
+        </Grid>
       </Grid>
       <Grid
-        item
+        container
+        gap={3}
         sx={{
-          flex: { xs: "1 0 15rem", md: "1 0 15rem" },
-          height: { xs: "5rem", md: "5rem" },
-          background: "transparent",
-          display: "flex", // Enable flexbox
-          justifyContent: "center", // Center horizontally
-          alignItems: "center", // Center vertically
+          background: themeValue === "dark" ? "black" : "white",
+          padding: "15px 30px",
+          minHeight: "100vh",
         }}
       >
-        <Box sx={{ marginTop: "60%" }}>
-          <StepperComponent />
-        </Box>
+        <Grid
+          item
+          sx={{
+            flex: { xs: "1 0 15rem", md: "1 0 15rem" },
+            height: { xs: "5rem", md: "5rem" },
+            background: "transparent",
+            display: "flex", // Enable flexbox
+            justifyContent: "center", // Center horizontally
+            alignItems: "center", // Center vertically
+          }}
+        >
+          <Box
+            component="img"
+            src="/profile.webp" // Path to your image in the public folder
+            alt="Description of image"
+            sx={{
+              marginTop: "60%",
+              borderRadius: "50%", // Makes the image circular
+              width: "200px", // Fixed width
+              height: "200px", // Fixed height
+              objectFit: "cover", // Ensures the image covers the area while maintaining aspect ratio
+            }}
+          />
+          <Box sx={{}} />
+        </Grid>
+        <Grid
+          item
+          sx={{
+            flex: { xs: "1 0 15rem", md: "1 0 15rem" },
+            height: { xs: "5rem", md: "5rem" },
+            background: "transparent",
+            display: "flex", // Enable flexbox
+            justifyContent: "center", // Center horizontally
+            alignItems: "center", // Center vertically
+          }}
+        >
+          <Box sx={{ marginTop: "60%" }}>
+            <Typography>Skills</Typography>
+            <StepperComponent />
+          </Box>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
